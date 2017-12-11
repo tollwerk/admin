e453c5923d730060d4f82e801dbc93ec6a292e7f
Joschi Kuphal <joschi@kuphal.net>
2017-12-11 10:09:21 +0100

    Reverted to Alias based .well-known directory

--- src/Admin/Infrastructure/Persistence/Apache.php
+++ src/Admin/Infrastructure/Persistence/Apache.php
@@ -99,6 +99,7 @@ class Apache
     public function __invoke(VhostInterface $vhost)
     {
         $files = [];
+        $httpsPorts = $vhost->getPorts(Vhost::PROTOCOL_HTTPS);
 
         $variables = $this->config;
         $variables['primary_domain'] = strval($vhost->getPrimaryDomain());
@@ -109,6 +110,8 @@ class Apache
             rtrim($this->config['dataroot'].DIRECTORY_SEPARATOR.$vhost->getDocroot(), DIRECTORY_SEPARATOR);
         $variables['configroot'] = $this->config['vhostroot'].DIRECTORY_SEPARATOR.$variables['primary_domain'];
         $variables['php_version'] = $vhost->getPhp();
+        $variables['challenge'] = rtrim(App::getConfig('certbot.challenge'), '/').'/';
+        $variables['ssl'] = !empty($httpsPorts);
 
         // If the virtual host should redirect
         if ($vhost->getRedirectUrl() !== null) {
@@ -127,7 +130,6 @@ class Apache
             TemplateService::render('apache_vhost.include', $variables));
 
         // If the HTTPS protocol is supported
-        $httpsPorts = $vhost->getPorts(Vhost::PROTOCOL_HTTPS);
         if (!empty($httpsPorts)) {
             $certbotConfig = $this->helper->vhostDirectory($vhost).DIRECTORY_SEPARATOR.'certbot.ini';
             $certbotService = CertbotServiceFactory::create($certbotConfig);
@@ -155,7 +157,8 @@ class Apache
                 TemplateService::render('certbot.ini', $variables));
 
             // Create the well-known symlink
-            $certbotService->prepare($vhost, $this->helper);
+            // 2017-12-11: Disabled again, see deprecation notice
+            // $certbotService->prepare($vhost, $this->helper);
 
             // Output a hint if the primary domain isn't certified
             if (!$primaryDomainIsCertified) {
